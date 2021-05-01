8011f08dca64202a13282dca913c32cf856b419e
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2021-05-01 10:39:25 +0200

    refactor: Refactor ReadLaterDataContext to proper naming, lowlatter casing start

--- ReadLater5.Repositories/Categories/CategoryRepository.cs
+++ ReadLater5.Repositories/Categories/CategoryRepository.cs
@@ -7,44 +7,44 @@ namespace ReadLater5.Repositories.Categories
 {
     public class CategoryRepository : ICategoryRepository
     {
-        private ReadLaterDataContext _ReadLaterDataContext;
+        private ReadLaterDataContext _readLaterDataContext;
         public CategoryRepository(ReadLaterDataContext readLaterDataContext) 
         {
-            _ReadLaterDataContext = readLaterDataContext;            
+            _readLaterDataContext = readLaterDataContext;            
         }
 
         public List<Category> GetCategories()
         {
-            return _ReadLaterDataContext.Categories.ToList();
+            return _readLaterDataContext.Categories.ToList();
         }
 
         public Category GetCategory(int Id)
         {
-            return _ReadLaterDataContext.Categories.Where(c => c.ID == Id).FirstOrDefault();
+            return _readLaterDataContext.Categories.Where(c => c.ID == Id).FirstOrDefault();
         }
 
         public Category GetCategory(string Name)
         {
-            return _ReadLaterDataContext.Categories.Where(c => c.Name == Name).FirstOrDefault();
+            return _readLaterDataContext.Categories.Where(c => c.Name == Name).FirstOrDefault();
         }
 
         public Category CreateCategory(Category category)
         {
-            _ReadLaterDataContext.Add(category);
-            _ReadLaterDataContext.SaveChanges();
+            _readLaterDataContext.Add(category);
+            _readLaterDataContext.SaveChanges();
             return category;
         }
 
         public void UpdateCategory(Category category)
         {
-            _ReadLaterDataContext.Update(category);
-            _ReadLaterDataContext.SaveChanges();
+            _readLaterDataContext.Update(category);
+            _readLaterDataContext.SaveChanges();
         }
 
         public void DeleteCategory(Category category)
         {
-            _ReadLaterDataContext.Categories.Remove(category);
-            _ReadLaterDataContext.SaveChanges();
+            _readLaterDataContext.Categories.Remove(category);
+            _readLaterDataContext.SaveChanges();
         }
     }
 }
