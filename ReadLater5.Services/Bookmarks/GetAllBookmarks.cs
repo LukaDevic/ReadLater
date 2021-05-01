e42f171a85b521c02abc5d187f57eba14f46b79b
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2021-05-01 13:52:18 +0200

    feature: Create GetAllBookmarks handler

---
+++ ReadLater5.Services/Bookmarks/GetAllBookmarks.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using ReadLater5.Entity;
+using ReadLater5.Repositories.Bookmarks;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReadLater5.Services.Bookmarks
+{
+    public static class GetAllBookmarks
+    {
+        public class Handler : IRequestHandler<Query, Result>
+        {
+            private readonly IBookmarksRepository _bookmarksRepository;
+            public Handler(IBookmarksRepository bookmarksRepository)
+            {
+                _bookmarksRepository = bookmarksRepository;
+            }
+
+            public async Task<Result> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var bookmarks = await _bookmarksRepository.GetBookmarksAsync();
+                return new Result(bookmarks);
+            }
+        }
+
+        public class Query : IRequest<Result> { }
+
+        public class Result
+        {
+            public Result(IList<Bookmark> bookmark)
+            {
+                Bookmarks = bookmark;
+            }
+
+            public IList<Bookmark> Bookmarks { get; }
+        }
+    }
+}
