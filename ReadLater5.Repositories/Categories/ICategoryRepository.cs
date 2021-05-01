52bd185a42aecddbff49c5c2d4a9a9ed8917c74b
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2021-05-01 10:37:48 +0200

    refactor: Reorder ICategory interface Methods to proper order

--- ReadLater5.Repositories/Categories/ICategoryRepository.cs
+++ ReadLater5.Repositories/Categories/ICategoryRepository.cs
@@ -5,10 +5,10 @@ namespace ReadLater5.Repositories.Categories
 {
     public interface ICategoryRepository
     {
-        Category CreateCategory(Category category);
         List<Category> GetCategories();
         Category GetCategory(int Id);
         Category GetCategory(string Name);
+        Category CreateCategory(Category category);
         void UpdateCategory(Category category);
         void DeleteCategory(Category category);
     }
