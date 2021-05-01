06296001b4cc856dfd803cae13e9cec271f33435
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2021-05-01 10:12:36 +0200

    feature: Add BookmarksController with mediatR in constructor

---
+++ ReadLater5/Controllers/BookmarksController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReadLater5.Controllers
+{
+    public class BookmarksController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public BookmarksController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
