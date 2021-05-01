f5e1024e8955276ec9c5a95d0969ac7f3c4f3f44
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2021-05-01 13:53:17 +0200

    feature: Add BookmarkViewModel

---
+++ ReadLater5/Models/Bookmark/BookmarkViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ReadLater5.Entity;
+using System.Collections.Generic;
+
+namespace ReadLater5.Models.Bookmark
+{
+    public class BookmarkViewModel
+    {
+        public string URL { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
