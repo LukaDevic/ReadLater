e4ca5e3524caa3a73a3fa2a9a32501a9eb1b8971
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2021-05-01 13:52:39 +0200

    feature: Create CreateBookmark handler

---
+++ ReadLater5.Services/Bookmarks/CreateBookmark.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using ReadLater5.Entity;
+using ReadLater5.Repositories.Bookmarks;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReadLater5.Services.Bookmarks
+{
+    public static class CreateBookmark
+    {
+        public class Handler : IRequestHandler<Command, Unit> 
+        {
+            private readonly IBookmarksRepository _bookmarksRepository;
+            public Handler(IBookmarksRepository bookmarksRepository)
+            {
+                _bookmarksRepository = bookmarksRepository;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+               await _bookmarksRepository.CrateBookmarkAsync(request.Bookmark);
+               return Unit.Value;
+            }
+        }
+
+        public class Command : IRequest<Unit>
+        {
+            public Command(Bookmark bookmark)
+            {
+                Bookmark = bookmark;
+            }
+
+            public Bookmark Bookmark { get; }
+        }
+    }
+}
