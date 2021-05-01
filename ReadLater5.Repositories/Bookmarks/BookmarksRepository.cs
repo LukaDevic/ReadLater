5dc99fcf6a584af5b9377473d70381fdab7cd22c
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2021-05-01 10:48:06 +0200

    feature: Add initiall BookmarksRepository and Interface

---
+++ ReadLater5.Repositories/Bookmarks/BookmarksRepository.cs
@@ -0,0 +1,42 @@
+﻿using ReadLater5.Data;
+using ReadLater5.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReadLater5.Repositories.Bookmarks
+{
+    public class BookmarksRepository : IBookmarksRepository
+    {
+        private ReadLaterDataContext _readLaterDataContext;
+        public BookmarksRepository(ReadLaterDataContext readLaterDataContext)
+        {
+            _readLaterDataContext = readLaterDataContext;
+        }
+
+        public IList<Bookmark> GetBookmarks()
+        {
+            return _readLaterDataContext.Bookmark.ToList();
+        }
+
+        public Bookmark GetBookmark(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bookmark CrateBookmark(Bookmark bookmark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBookmark(Bookmark bookmark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBookmark(Bookmark bookmark)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ReadLater5.Repositories/Bookmarks/IBookmarksRepository.cs
@@ -0,0 +1,14 @@
+﻿using ReadLater5.Entity;
+using System.Collections.Generic;
+
+namespace ReadLater5.Repositories.Bookmarks
+{
+    public interface IBookmarksRepository
+    {
+        IList<Bookmark> GetBookmarks();
+        Bookmark GetBookmark(int id);
+        Bookmark CrateBookmark(Bookmark bookmark);
+        void UpdateBookmark(Bookmark bookmark);
+        void DeleteBookmark(Bookmark bookmark);
+    }
+}
